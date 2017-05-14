36428a421f6ee477efac1c844873ebf49a372573
Muhammad A Molla <muhammadamolla@Muhammads-MacBook-Pro-4.local>
2017-05-14 17:58:21 -0400

    WeDriver

--- src/test/java/APi_Testing/com/Rtetpaxo/qa/Test.java
+++ src/test/java/APi_Testing/com/Rtetpaxo/qa/Test.java
@@ -1,10 +1,21 @@
 package APi_Testing.com.Rtetpaxo.qa;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("www.google.com");
 
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
 		System.out.println("test");
 	}
 
